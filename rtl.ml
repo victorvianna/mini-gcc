f1d604c16a042dce000c9def51d84daede8f1e6c
victorvianna <victor.vianna10@gmail.com>
2019-02-21 21:43:55 +0100

    [rtl] Init rtl

--- rtl.ml
+++ rtl.ml
@@ -7,9 +7,32 @@ let generate i =
   graph := Label.M.add l i !graph;
   l
 
+let rec expr (e:Ttree.expr) destr destl = match e.expr_node with
+  | Ttree.Econst e -> generate (Econst(e, destr, destl))  
 
-let program (ttree:Ttree.file) =
-  {funs = []}
+
+let rec stmt (s:Ttree.stmt) destl retr exitl = match s with
+  | Ttree.Sreturn e -> 
+    begin
+      match e.expr_node with
+      | Ttree.Econst e -> generate (Econst (e, retr, exitl))
+    end
+let deffun (f:Ttree.decl_fun) = 
+  let r = Register.fresh () in 
+  let l = Label.fresh () in
+  let fun_entry = stmt (Ttree.Sblock(f.fun_body)) l r l in
+  {
+    fun_name = f.fun_name;
+    fun_result = r;
+    fun_exit = l;
+    fun_entry = fun_entry;
+    fun_formals = [];
+    fun_locals = Register.S.empty;
+    fun_body = !graph;
+  }
+
+let program (ttree:Ttree.file) = 
+  {funs = List.map deffun ttree.funs}
 
 
 
--- rtl.mli
+++ rtl.mli
@@ -1,7 +1,17 @@
 open Rtltree
 
-val program: Ttree.file -> Rtltree.file
+val program: Ttree.file -> Rtltree.file 
 
+val deffun: Ttree.decl_fun -> Rtltree.deffun
+
+val generate: Rtltree.instr -> label
+
+val expr: Ttree.expr -> register -> label -> label  
+
+val stmt: Ttree.stmt -> label -> register -> label -> label
+
+
+val deffun: Ttree.decl_fun -> Rtltree.deffun
 
 
 
