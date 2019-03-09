cd701723557d5b5dedf2a4dbe80a03ec7ffb4970
Cauim de Souza Lima <cauimsouza@gmail.com>
2019-03-09 16:58:36 +0100

    [linear] Translate ltl operands to x86-64 operands

--- linear.ml
+++ linear.ml
@@ -1,3 +1,7 @@
+open X86_64
+
+exception Error of string
+   
 let visited_labels = Hashtbl.create 17
 type instr = Code of X86_64.text | Label of Label.t
 let code = ref []
@@ -6,6 +10,11 @@ let emit_wl instr = code := Code instr :: !code
 let labels = Hashtbl.create 17
 let need_label label = Hashtbl.add labels label ()
 
+let operand ltl_operand =
+  match ltl_operand with
+  | Ltltree.Reg r -> reg (register64 r)
+  | Ltltree.Spilled offset -> ind ~ofs:(offset) rbp 
+  
 let rec lin ltl_map label =
   if not (Hashtbl.mem visited_labels label) then begin
     Hashtbl.add visited_labels label ();
@@ -16,6 +25,6 @@ let rec lin ltl_map label =
   end
 
 and instr ltl_map label = function
-  | Econst (n, r, l1) ->
-      emit l (movq (imm32 n) (operand r)); lin ltl_map l1
-  ...
+  | Ltltree.Econst (n, r, l1) ->
+      emit label (movq (imm32 n) (operand r)); lin ltl_map l1
+  | _ -> raise (Error "undefined")
