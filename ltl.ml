053bdfd45468888312378ff2580067f5ce5c7e41
Cauim de S. Lima <cauimsouza@gmail.com>
2019-03-16 15:40:55 +0100

    Implement George-Appel coloring

--- ltl.ml
+++ ltl.ml
@@ -19,10 +19,218 @@ type coloring = color Register.map
 
 let graph = ref Label.M.empty
 
+let original_intfs = ref Register.M.empty
+
 let generate i = (* should move this to a common file later *)
   let l = Label.fresh () in
   graph := Label.M.add l i !graph;
   l
+
+let k = Register.S.cardinal Register.allocatable
+let spilled_regs = ref 0
+
+let no_preference_edge r r_arcs =
+  Register.S.is_empty r_arcs.prefs
+
+let reg_degree g r =
+  try
+    let r_arcs = Register.M.find r g in
+    Register.S.cardinal r_arcs.prefs + Register.S.cardinal r_arcs.intfs
+  with Not_found -> raise (Error "Empty interference graph")
+
+let minimal_degree_reg g =
+  let min_r = ref (fst (Register.M.choose g)) in
+  let min_degree = ref (reg_degree g !min_r) in
+  let update_best r r_arcs =
+    let r_degree = reg_degree g r in
+    if r_degree < !min_degree then
+      begin
+        min_r := r;
+        min_degree := r_degree
+      end
+  in
+  Register.M.iter update_best g;
+  !min_r
+
+let satisfies_crit_when_v2_is_hw g v1 v2 =
+  let v1_arcs = Register.M.find v1 g in
+  let v2_arcs = Register.M.find v2 g in
+  let candidates =
+    Register.S.filter
+      (fun s -> reg_degree g s >= k || Register.is_pseudo s) v1_arcs.intfs
+  in
+  Register.S.for_all (fun s -> Register.S.mem s v2_arcs.intfs) candidates
+
+let satisfies_crit_when_v2_is_pseudo g v1 v2 =
+  let v1_arcs = Register.M.find v1 g in
+  let v2_arcs = Register.M.find v2 g in
+  let candidates =
+    Register.S.filter
+      (fun s -> reg_degree g s >= k || Register.is_hw s) v1_arcs.intfs
+  in
+  Register.S.for_all (fun s -> Register.S.mem s v2_arcs.intfs) candidates
+
+let find_george_edge g =
+  let may_be_merged_with v1 v2 =
+    if Register.is_hw v2 then satisfies_crit_when_v2_is_hw g v1 v2
+    else satisfies_crit_when_v2_is_pseudo g v1 v2
+  in
+  let has_george_edge v =
+    let v_arcs = Register.M.find v g in
+    let candidates = Register.S.filter (may_be_merged_with v) v_arcs.prefs in
+    not (Register.S.is_empty candidates)
+  in
+  let v1_candidates =
+    Register.M.filter (fun r a -> has_george_edge r) g
+  in
+  if Register.M.is_empty v1_candidates then None
+  else
+    let v1, v1_arcs = Register.M.choose v1_candidates in
+    let v2_candidates = Register.S.filter (may_be_merged_with v1) v1_arcs.prefs in
+    Some (v1, Register.S.choose v2_candidates)
+
+let forget_pref_edges g v =
+  let v_arcs = Register.M.find v g in
+  let new_graph = ref (Register.M.add v {v_arcs with prefs = Register.S.empty} g) in
+  Register.S.iter
+    (fun s -> let s_arcs = Register.M.find s !new_graph in
+      let new_prefs = Register.S.remove v s_arcs.prefs in
+      new_graph := Register.M.add s {s_arcs with prefs = new_prefs} !new_graph
+    ) v_arcs.prefs;
+  !new_graph
+
+let minimal_cost_reg g =
+  let cost v =
+    let n_uses = 1 (*Register.M.find v n_uses_graph*) in
+    let degree = reg_degree g v in
+    (float_of_int n_uses) /. (float_of_int degree)
+  in
+  let best_v = ref (fst (Register.M.choose g)) in
+  let min_cost = ref (cost !best_v) in
+  Register.M.iter
+    (fun r a ->
+       let r_cost = cost r in
+       if r_cost < !min_cost then
+         begin min_cost := r_cost; best_v := r end
+    ) g;
+  !best_v
+
+let remove_reg g v =
+  let v_arcs = Register.M.find v g in
+  let new_graph = ref (Register.M.remove v g) in
+  Register.S.iter
+    (fun s ->
+       let s_arcs = Register.M.find s !new_graph in
+       let new_intfs = Register.S.remove v s_arcs.intfs in
+       let new_prefs = Register.S.remove v s_arcs.prefs in
+       new_graph := Register.M.add s {prefs = new_prefs; intfs = new_intfs} !new_graph
+    ) (Register.S.union v_arcs.intfs v_arcs.prefs);
+  !new_graph
+
+let lookup c r =
+  if Register.is_hw r then Reg r else Register.M.find r c
+
+let color_possible g c v =
+  if Register.is_hw v then Some v
+  else
+    let v_arcs = Register.M.find v g in
+    let neighbour_colors = ref (Register.S.empty) in
+    Register.S.iter
+      (fun s -> match lookup c s with
+         | Reg hr -> neighbour_colors := Register.S.add hr !neighbour_colors
+         | Spilled _ -> ()
+      ) v_arcs.intfs;
+    let possible_colors = Register.S.diff Register.allocatable (Register.M.find v !original_intfs)
+    in
+    Register.S.choose_opt (Register.S.diff possible_colors !neighbour_colors)
+
+(* function to keep only interf edges in case there are both *)
+let clean_double edges =
+  {intfs = edges.intfs; prefs = Register.S.diff edges.prefs edges.intfs}
+
+let fusion g v1 v2 =
+  let soldado = Register.M.find v1 !original_intfs in
+  let cauim = Register.M.find v2 !original_intfs in
+  original_intfs := Register.M.add v2 (Register.S.union soldado cauim) !original_intfs;
+  let v1_arcs = Register.M.find v1 g in
+  let v1_new_intfs = Register.S.remove v2 v1_arcs.intfs in
+  let v1_new_prefs = Register.S.remove v2 v1_arcs.prefs in
+  let v2_arcs = Register.M.find v2 g in
+  let v2_new_intfs = Register.S.remove v1 v2_arcs.intfs in
+  let v2_new_prefs = Register.S.remove v1 v2_arcs.prefs in
+  let v2_new_intfs = Register.S.union v1_new_intfs v2_new_intfs in
+  let v2_new_prefs = Register.S.union v1_new_prefs v2_new_prefs in
+  let new_graph = ref g in
+  new_graph := Register.M.add v2 {prefs = v2_new_prefs; intfs = v2_new_intfs} !new_graph;
+  new_graph := Register.M.remove v1 !new_graph;
+  let transform_v1 = (fun v -> if v = v1 then v2 else v) in
+  (* convert v1 to v2 in every edge list *)
+  Register.M.iter (fun s s_arcs ->
+      let new_intfs = Register.S.map transform_v1 s_arcs.intfs in
+      let new_prefs = Register.S.map transform_v1 s_arcs.prefs in
+      new_graph := Register.M.add s {prefs = new_prefs; intfs = new_intfs} !new_graph
+    ) !new_graph;
+  (* clean double edges that may have been created *)
+  Register.M.map (fun arcs -> clean_double arcs) !new_graph
+
+let rec simplify g =
+  let no_pref_edges_graph = Register.M.filter no_preference_edge g in
+  if not (Register.M.is_empty no_pref_edges_graph) then
+    begin
+      let min_degree_r = minimal_degree_reg no_pref_edges_graph in
+      let min_degree = reg_degree no_pref_edges_graph min_degree_r in
+      if min_degree < k then select g min_degree_r
+      else coalesce g
+    end
+  else coalesce g
+and
+  coalesce g =
+  match find_george_edge g with
+  | Some (v1, v2) ->
+    let g = fusion g v1 v2 in
+    let c = simplify g in
+    let c = Register.M.add v1 (lookup c v2) c in
+    c
+  | None ->
+    freeze g
+and
+  freeze g =
+  if (Register.M.is_empty g) then spill g
+  else
+    let min_r = minimal_degree_reg g in
+    let min_degree = reg_degree g min_r in
+    if min_degree < k then
+      let g = forget_pref_edges g min_r in
+      simplify g
+    else
+      spill g
+and
+  spill g =
+  if Register.M.is_empty g then
+    Register.M.empty
+  else
+    let v = minimal_cost_reg g in
+    select g v
+and
+  select g v =
+  let g_v = remove_reg g v in
+  let c = simplify g_v in
+  match color_possible g c v with
+  | Some x -> Register.M.add v (Reg x) c
+  | None ->
+    let word_size = 8 in
+    incr spilled_regs;
+    Register.M.add v (Spilled (!spilled_regs * -word_size)) c
+
+let color_george_appel graph =
+  original_intfs := Register.M.empty;
+  Register.M.iter
+    (fun s s_arcs ->
+       let intfs = s_arcs.intfs in
+       let hw_intfs = Register.S.filter (fun r -> Register.is_hw r) intfs in
+       original_intfs := Register.M.add s hw_intfs !original_intfs
+    ) graph;
+  simplify graph
 
 let make l_info =
   let interf_graph = ref Register.M.empty in
@@ -112,7 +320,7 @@ let fourth_crit todo pcolors_map graph color_map =
 let third_crit todo pcolors_map graph color_map =
   let is_colored_and_possible pcolors r =
     if not (Register.M.mem r color_map) then false
-    else match Register.M.find r color_map with
+    else match lookup color_map r with
       | Reg hw_reg -> Register.S.mem hw_reg pcolors
       | _ -> false in
   let satisfies_crit r =
@@ -128,7 +336,7 @@ let third_crit todo pcolors_map graph color_map =
     let pcolors = Register.M.find r pcolors_map in
     let colored_prefs = Register.S.filter (is_colored_and_possible pcolors) prefs in
     let colored_pref = Register.S.choose colored_prefs in
-    let c = match Register.M.find colored_pref color_map with
+    let c = match lookup color_map colored_pref with
       | Reg col -> col
       | _ -> raise (Error "It should had a physical register associated")
     in Some (r, c)
@@ -202,9 +410,6 @@ let color_graph graph =
     | None -> let r = Register.S.choose !todo in spill r
     | Some (r, c) -> set_reg_color r c
   done; !color_map, !n_regs_stack
-
-let lookup c r =
-  if Register.is_hw r then Reg r else Register.M.find r c
 
 let translate_Eload r1 i r2 l c =
   let op1 = lookup c r1 in
@@ -369,9 +574,8 @@ let instr c frame_size n_pars = function
 let translate_fun (f:Ertltree.deffun) =
   let l_info = Ertl.liveness f.fun_body in
   let interf_graph = make l_info in
-  let colors = color_graph interf_graph in
-  let color_map = fst colors in
-  let frame_size = snd colors in
+  let color_map = color_george_appel interf_graph in
+  let frame_size = !spilled_regs in
   (* we have to clear the graph because otherwise we might have
      conflict if two different functions have the same label *)
   let () = graph := Label.M.empty in
